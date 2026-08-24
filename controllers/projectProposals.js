f0bec13d612355e43738716d59c5353a007baf59
ma-all <marwaalbaloushi21@gmail.com>
2026-08-24 22:06:18 +0300

    added delete function

--- controllers/projectProposals.js
+++ controllers/projectProposals.js
@@ -36,6 +36,15 @@ const update = async (req, res) => {
     }
 }
 
+const deleteProjectProposal = async (req, res) => {
+    try {
+        const deletedProjectProposal = await ProjectProposal.findByIdAndDelete(req.params.projectProposalId)
+        res.status(200).json(deletedProjectProposal)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
 module.exports = {
-    create, index, show, update
+    create, index, show, update, deleteProjectProposal,
 }
