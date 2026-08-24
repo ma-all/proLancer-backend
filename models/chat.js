b7888cac0eb082350a37e20849d8f03b880a50d4
ma-all <marwaalbaloushi21@gmail.com>
2026-08-24 22:29:05 +0300

    created chat model

---
+++ models/chat.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    msg: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true })
+
+const chatSchema = new mongoose.Schema({
+    businessOwnerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    developerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    messages: [messageSchema],
+}, { timestamps: true })
+
+const Chat = mongoose.model('Chat', chatSchema)
+
+module.exports = Chat
