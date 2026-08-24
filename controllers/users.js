5180086e513983ab992df131d9c80b7abc9fa2c7
Rawan-haji <rawanjan02@gmail.com>
2026-08-24 16:09:28 +0300

    create the user models i add the ategory array and skill aray

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,18 @@
 const User = require('../models/user')
 
-const index = async (req, res) => {
-   const users = await User.find()
-   res.json(users)
-}
+// const index = async (req, res) => {
+//    const users = await User.find()
+//    res.json(users)
+// }
 
-module.exports = {
-    index,
-}
+
+
+
+
+
+
+ module.exports = {
+//     index,
+ }
+
+
