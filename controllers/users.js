0ba638d217bf279ff8856b2c253f47b0caf3a1d9
ma-all <marwaalbaloushi21@gmail.com>
2026-08-25 14:47:24 +0300

    added delete function for github links in the profile

--- controllers/users.js
+++ controllers/users.js
@@ -39,10 +39,23 @@ const update = async(req, res)=>{
     }
 }
 
+const deleteGithubLink = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId)
+        if (!user)
+            return res.status(404).json({message:'user not found'})
+
+        user.githubUrl = ''
+        await user.save()
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
 
 
 module.exports = {
-    create,  show, update,
+    create,  show, update, deleteGithubLink,
  }
 
 //CODE GRAVEYARD
