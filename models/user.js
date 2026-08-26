943a87c0b4d472da486aaae6bdc62d37ee1c1847
Rawan-haji <rawanjan02@gmail.com>
2026-08-26 12:34:15 +0300

    fixed username

--- models/user.js
+++ models/user.js
@@ -2,57 +2,60 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     username: {
-        type: String,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
         required: true,
     },
     password: {
         type: String,
         required: true,
     },
-     email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    role:{
-        type:String,
-        required:true,
+    role: {
+        type: String,
+        required: true,
         enum: ['Business Owner', 'Developer']
     },
 
     businessDescription: {
         type: String,
-    
+
     },
-    businessCategory:{
-        type:String,
+    businessCategory: {
+        type: String,
         enum: ['Cafe', 'Restaurant', 'Bakery', 'Catering', 'Jewelry', 'Cosmetics', 'Clothing', 'Footware', 'Photo & Videography', 'Event Planning', 'Beauty Salon', 'Spa', 'Tech Equipment', 'Equipment & Machinery', 'Gym', 'Art Studio', 'Handmade Crafts', 'Interior Design', 'Furniture & Home Decor', 'Architecture', 'Stationary', 'Medical Clinic', 'Pharmacy', 'Dental Clinic', 'Fitness & Health', 'Other'],
         default: 'Other'
     },
- 
-    developerDescription:{
-        type:String
+
+    developerDescription: {
+        type: String
     },
 
     githubUrl: [{
-        type:String
+        type: String
     }],
 
     deployedLinks: [{
-        type:String
+        type: String
     }],
 
     skills: [{
-        type:String,
-        enum:['HTML', 'CSS', 'JavaScript', 'TypeScript','React','BootStrap', 'SCSS', 'Angular', 'Node.js', 'Python', 'Java', 'C#', 'Express.js', 'Django', 'MongoDB', 'PHP', 'PostgreSQL', 'JWT Authentication', 'WebSockets', 'GitHub', 'Firebase', 'Cloudinary', 'Stripe']
+        type: String,
+        enum: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'React', 'BootStrap', 'SCSS', 'Angular', 'Node.js', 'Python', 'Java', 'C#', 'Express.js', 'Django', 'MongoDB', 'PHP', 'PostgreSQL', 'JWT Authentication', 'WebSockets', 'GitHub', 'Firebase', 'Cloudinary', 'Stripe']
     }],
 
     projectProposals: [
-       { type: mongoose.Schema.Types.ObjectId,
-        ref: 'ProjectProposal'}
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'ProjectProposal'
+        }
     ]
 
-}, {timestamps: true})
+}, { timestamps: true })
 
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
