3b3547c317e40918d4abe10077e0f12debe61f2f
ma-all <marwaalbaloushi21@gmail.com>
2026-08-30 10:21:43 +0300

    added socket in the server for chats

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
 const morgan = require('morgan')
+const http = require('http')
+const { Server } = require('socket.io')
+
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost: 5174', //im not sure but i think i'll have to change this later for deployment
+  }
+})
 
 const PORT = process.env.PORT ? process.env.PORT : "3000"
 
@@ -66,7 +75,15 @@ app.post('/chat/:chatId/messages', verifyToken, chatCtrl.sendMessage)
 app.post('/payment/createPayment', verifyToken, paymentCtrl.create)
 app.post('/payment/confirmPayment', verifyToken, paymentCtrl.confirm)
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('Socket connected: ', socket.id)
+
+  socket.on('disconnected', () => {
+    console.log('Socket disconnected: ', socket.id)
+  })
+})
+
+server.listen(PORT, () => {
   console.log(`The express app is ready on port ${PORT}! 😀`)
 })
 
